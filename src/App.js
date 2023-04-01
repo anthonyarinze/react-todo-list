565a289b340c016d482ccf7d1dfb9b8d006389ba
Anthony Arinze <anthonyarinze28@gmail.com>
2023-04-01 11:43:50 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todoValue, setTodoValue] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 0,
+      value: "Test todo element 1",
+      isDone: false,
+    },
+  ]);
+
+  const handleChange = (e) => {
+    //Updates local component state
+    setTodoValue(e.target.value);
+  };
+
+  const clearInput = () => {
+    //Clear existing value in input
+    document.getElementById("todoValueInput").value = "";
+    //Update local component state
+    setTodoValue("");
+  };
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length,
+        value: todoValue,
+        isDone: false,
+      },
+    ]);
+    clearInput();
+  };
+
+  const setTodoAsDone = (id) => {
+    const todosNew = [...todos];
+    todosNew.map((t) => {
+      if (t.id === id) {
+        t.isDone = !t.isDone;
+      }
+      return t;
+    });
+    setTodos(todosNew);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p className="title">Testing</p>
+      <div className="inputDiv">
+        <input
+          type="input"
+          id="todoValueInput"
+          className="searchBar"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="submitButton" onClick={() => addTodo()}>
+          Add Task
+        </button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => setTodoAsDone(todo.id)}>
+            {todo.value}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
